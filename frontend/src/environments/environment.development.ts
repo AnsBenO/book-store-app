a4f4fd11440bbaed4940603dd724bf336baaad6c
AnsBenO <ans.benomar@gmail.com>
2024-08-08 20:55:52 +0100

    nginx configuration

--- frontend/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  booksApiUrl: 'http://localhost:8081/api',
-};
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  booksApiUrl: 'http://localhost:8989/books/api',
-};
